c518c820249fe21ad855fdbd90aaa5ef877c2cb1
Davidlibreros07 <davidlibrerosa@gmail.com>
2026-04-17 13:00:56 -0500

    Segundo flujo (contenedorizado)

--- DataAcquisition_Semana4S1/venv/Scripts/activate.bat
+++ DataAcquisition_Semana4S1/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\jhony\Documents\Pryto Integrador files\DataAcquisition_Semana4S1\venv
+set VIRTUAL_ENV=C:\Users\USUARIO\OneDrive\Desktop\semestre 5\Proyecto integrador 1\Backend repo\Backend-BESTIA\DataAcquisition_Semana4S1\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
